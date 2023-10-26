743270d0a8a640c8032b00b3a044d9ce26762b15
Andres M. Prieto <andresprieto2703@gmail.com>
2023-10-26 02:17:34 -0500

    Functional calculator
    Adding functionality

--- src/calc/index.js
+++ src/calc/index.js
@@ -1,6 +1,13 @@
+let floatingPoint = true;
 let string = "";
+
 const operation = {
-  0: (value) => (string += value),
+  0: (value) => {
+    string === "" ||
+    (!isFinite(string.charAt(string.length - 1)) && !string.includes("."))
+      ? (string += value + ".")
+      : (string += value);
+  },
   1: (value) => (string += value),
   2: (value) => (string += value),
   3: (value) => (string += value),
@@ -10,19 +17,53 @@ const operation = {
   7: (value) => (string += value),
   8: (value) => (string += value),
   9: (value) => (string += value),
-  "+": () => (string += "+"),
-  "-": () => (string += "-"),
-  x: () => (string += "*"),
-  "/": () => (string += "/"),
-  ".": () => (string += "."),
-  "=": () => eval(string),
-  "del": () => (string = string.slice(0, -1)),
+  "+": () => {
+    string === "" || !isFinite(string.charAt(string.length - 1))
+      ? (string = string)
+      : (string += "+");
+    floatingPoint = true;
+  },
+  "-": () => {
+    string === "" || !isFinite(string.charAt(string.length - 1))
+      ? (string = string)
+      : (string += "-");
+    floatingPoint = true;
+  },
+  x: () => {
+    string === "" || !isFinite(string.charAt(string.length - 1))
+      ? (string = string)
+      : (string += "*");
+    floatingPoint = true;
+  },
+  "/": () => {
+    string === "" || !isFinite(string.charAt(string.length - 1))
+      ? (string = string)
+      : (string += "/");
+    floatingPoint = true;
+  },
+  ".": () => {
+    if (string === "" || !isFinite(string.charAt(string.length - 1))) {
+      string += "0.";
+    } else if (floatingPoint) {
+      string += ".";
+      floatingPoint = false;
+    }
+  },
+  "=": () => {
+    string = `${eval(string)}`;
+  },
+  del: () => {
+    string = string.slice(0, -1);
+  },
   reset: () => (string = ""),
 };
 
 const registerCalc = (value, target) => {
-  operation[value](value);
-  target.textContent = string;
+  try {
+    operation[value.toLowerCase()](value);
+  } catch (error) {}
+
+  string === "" ? (target.textContent = "0") : (target.textContent = string);
 };
 
 export { registerCalc };
